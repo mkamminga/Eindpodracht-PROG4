a0febe3228128d16dac80df81d14fd7b4b10ced4
mkamminga <mjkammin@student.avans.nl>
2014-10-17 20:28:43 +0200

    Final version for merge

--- Eindopdracht PROG4/Controllers/BookingController.cs
+++ Eindopdracht PROG4/Controllers/BookingController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Eindopdracht_PROG4.Models;
+using Eindopdracht_PROG4.Models.Entities;
+using Eindopdracht_PROG4.Models.ViewModels;
 
 namespace Eindopdracht_PROG4.Controllers
 {
     public class BookingController : Controller
     {
+        private EntityBookings entityBooking    = new EntityBookings();
+        private EntityRooms entityRooms         = new EntityRooms();
         //
         // GET: /Booking/
 
@@ -15,8 +20,26 @@ namespace Eindopdracht_PROG4.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            BookingViewModel bVm = new BookingViewModel(null);
+            bVm.Rooms = entityRooms.getAll();
 
-        public ActionResult Create()
+            return View(bVm);
+        }
+        [HttpPost]
+        public ActionResult Add(Booking booking)
+        {
+            return View();
+        }
+
+        public ActionResult AddGuests(int bookingId)
+        {
+            return View();
+        }
+
+        public ActionResult Confirm(int bookingId)
         {
             return View();
         }
